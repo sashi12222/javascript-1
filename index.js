6ce65558d48c0815886da5769b788cb5245d0df1
sashi12222 <sashiraj1222@gmail.com>
2022-10-08 23:40:23 +0530

    practice/hustling

---
+++ index.js
@@ -0,0 +1,44 @@
+// document.getElementById("count").innerText=30;
+let count=0;
+console.log(count)
+// create a variable, myAge, and set its value to your age
+let myAge=19;
+console.log(myAge);
+let count1=5*3;
+console.log(count1);
+let firstBatch=10;
+count2=firstBatch;
+console.log(count2);
+let myage1=19;
+let humandogratio=7;
+let myDogAge=myage1*humandogratio;
+console.log(myDogAge);
+//Create a variable, bonusPoints. Intialize it as 50. Increase it to 100. 
+// Decrease it down to 25, and then finally increase it to 70
+//Console.logthe value after the each step
+let bonusPoints=50;
+bonusPoints=bonusPoints+50;
+console.log(bonusPoints)
+bonusPoints=bonusPoints-75;
+console.log(bonusPoints);
+bonusPoints=bonusPoints+45;
+console.log(bonusPoints);
+// document.getElementById("count").innerText=90;
+// function increment(){
+//     console.log("Hello World");
+// }
+function increment(){
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+increment();
+
+increment();
+
+function madarchod(){
+    console.log(42);
+}
+madarchod();
